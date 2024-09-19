b8a24440880416cb07e57e06cadf44707edaaaa6
Ivan-Lomando <ilomandofernandez@gmail.com>
2024-09-18 21:42:55 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+  
+  const reset = () => setCount(0);
+
+  return (
+    <div className="counter">
+      <h1>Contador: {count}</h1>
+      <div>
+        <button onClick={increment}>Incrementar</button>
+        <button onClick={decrement}>Decrementar</button>
+        <button onClick={reset}>Reiniciar</button>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter /> 
     </div>
   );
 }
